06cce4713335edff6febb189dbfca8ace86e82db
Matthew Slauson <matthew@slauson.io>
2024-02-23 15:00:14 -0600

    fix

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 currentPath="$(pwd)"
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager#
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
 sudo nix-channel --update
 nix-shell '<home-manager>' -A install
 
