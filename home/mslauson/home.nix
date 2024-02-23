f971318f17b9ba34a1cae17ae5b1e5cc89a4cae7
Matthew Slauson <matthew@slauson.io>
2024-02-23 15:34:00 -0600

    ud

--- home/mslauson/home.nix
+++ home/mslauson/home.nix
@@ -19,16 +19,18 @@
   home.stateVersion = "23.11"; # Please read the comment before changing.
   programs.lazygit = {
     enable = true;
-    gui.theme = {
-      lightTheme = false;
-      activeBorderColor = [ "#a6e3a1" "bold" ];
-      inactiveBorderColor = [ "#cdd6f4" ];
-      optionsTextColor = [ "#89b4fa" ];
-      selectedLineBgColor = [ "#313244" ];
-      selectedRangeBgColor = [ "#313244" ];
-      cherryPickedCommitBgColor = [ "#94e2d5" ];
-      cherryPickedCommitFgColor = [ "#89b4fa" ];
-      unstagedChangesColor = [ "red" ];
+    settings = {
+      gui.theme = {
+        lightTheme = false;
+        activeBorderColor = [ "#a6e3a1" "bold" ];
+        inactiveBorderColor = [ "#cdd6f4" ];
+        optionsTextColor = [ "#89b4fa" ];
+        selectedLineBgColor = [ "#313244" ];
+        selectedRangeBgColor = [ "#313244" ];
+        cherryPickedCommitBgColor = [ "#94e2d5" ];
+        cherryPickedCommitFgColor = [ "#89b4fa" ];
+        unstagedChangesColor = [ "red" ];
+      };
     };
   };
   programs.zoxide.enable = true;
