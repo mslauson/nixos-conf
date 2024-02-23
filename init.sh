deeb3cda7f32b06b50e0652bed3326fe6d8b386c
Matthew Slauson <matthew@slauson.io>
2024-02-23 14:35:09 -0600

    add home manager to script

--- init.sh
+++ init.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 currentPath="$(pwd)"
 
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-
-sudo nix-channel --update
+# sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+#
+# sudo nix-channel --update
+# nix-shell '<home-manager>' -A install
 
 for file in configuration.nix; do
 	sudo rm -rf /etc/nixos/$file
 	sudo ln -s "$currentPath"/"$file" /etc/nixos/"$file"
 done
+
+# Home manager
+
+rm -rf /home/mslauson/.config/home-manager
+ln -s "$currentPath"/home/mslauson /home/mslauson/.config/home-manager
