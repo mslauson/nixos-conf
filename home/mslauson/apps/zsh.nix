2b5a252800c48794872bada5698307864b1aa853
Matthew Slauson <matthew@slauson.io>
2024-02-24 14:21:20 -0600

    ffs

--- home/mslauson/apps/zsh.nix
+++ home/mslauson/apps/zsh.nix
@@ -8,20 +8,38 @@
         extended = true;
         save = 25000;
       };
-    zplug = {
+    # zplug = {
+    #   enable = true;
+    #   plugins = [
+    #     { name="zsh-users/zsh-autosuggestions"; }
+    #     { name="zsh-users/zsh-completions"; }
+    #     { name="zsh-users/zsh-syntax-highlighting"; }
+    #     { name="Aloxaf/fzf-tab"; }
+    #     { name="akash329d/zsh-alias-finder"; }
+    #     { name="KulkarniKaustubh/fzf-dir-navigator"; }
+    #     { name="joshskidmore/zsh-fzf-history-search"; }
+    #     { name="reegnz/jq-zsh-plugin"; }
+    #
+    #     { name="ChrisPenner/session-sauce"; }
+    #     { name="nix-community/nix-zsh-completions"; }
+    #   ];
+    #
+    # };
+
+    oh-my-zsh = {
       enable = true;
       plugins = [
-        { name="zsh-users/zsh-autosuggestions"; }
-        { name="zsh-users/zsh-completions"; }
-        { name="zsh-users/zsh-syntax-highlighting"; }
-        { name="Aloxaf/fzf-tab"; }
-        { name="akash329d/zsh-alias-finder"; }
-        { name="KulkarniKaustubh/fzf-dir-navigator"; }
-        { name="joshskidmore/zsh-fzf-history-search"; }
-        { name="reegnz/jq-zsh-plugin"; }
+        "zsh-users/zsh-autosuggestions"
+        "zsh-users/zsh-completions"
+        "zsh-users/zsh-syntax-highlighting"
+        "Aloxaf/fzf-tab"
+        "akash329d/zsh-alias-finder"
+        "KulkarniKaustubh/fzf-dir-navigator"
+        "joshskidmore/zsh-fzf-history-search"
+        "reegnz/jq-zsh-plugin"
 
-        { name="ChrisPenner/session-sauce"; }
-        { name="nix-community/nix-zsh-completions"; }
+        "ChrisPenner/session-sauce"
+        "nix-community/nix-zsh-completions"
       ];
 
     };
