abca6a9d5cec877b47a31273a13245a788728f6b
Matthew Slauson <matthew@slauson.io>
2024-02-24 14:27:08 -0600

    ffs

--- home/mslauson/apps/zsh.nix
+++ home/mslauson/apps/zsh.nix
@@ -8,15 +8,19 @@
       save = 25000;
     };
     plugins = [
+      # {
+      #   # will source zsh-autosuggestions.plugin.zsh
+      #   name = "zsh-autosuggestions";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "zsh-users";
+      #     repo = "zsh-autosuggestions";
+      #     # rev = "v0.4.0";
+      #     # sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+      #   };
+      # }
       {
-        # will source zsh-autosuggestions.plugin.zsh
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          # rev = "v0.4.0";
-          # sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-        };
+        name = "zsh-syntax-highlighting";
+        src = pkgs.zsh-syntax-highlighting;
       }
     ];
     # zplug = {
