9d56b7dd5f0bf8e16259bf94828e82a67d941081
Matthew Slauson <matthew@slauson.io>
2024-02-24 15:37:05 -0600

    f

--- home/mslauson/apps/git.nix
+++ home/mslauson/apps/git.nix
@@ -6,10 +6,6 @@
     userName = "Matthew Slauson";
     userEmail = "matthew@slauson.io";
     signing = { key = "3AB57AD2F78683DF9E9D0659EF6F329D8D6E51C5"; };
-    diff-so-fancy = {
-      enable = true;
-      markEmptyLines = true;
-    };
     aliases = {
       st = "status";
       co = "checkout";
@@ -17,15 +13,21 @@
       f = "fetch";
       m = "merge";
       cam = "commit -am";
-      lg = "log --graph --format='%Cred%h%Creset  %<|(15) %C(white)%s %<|(35) %Creset %Cgreen(%cr)%<|(55)  %C(blue)<%an>%Creset%C(yellow)%d%Creset'";
+      lg =
+        "log --graph --format='%Cred%h%Creset  %<|(15) %C(white)%s %<|(35) %Creset %Cgreen(%cr)%<|(55)  %C(blue)<%an>%Creset%C(yellow)%d%Creset'";
     };
     extraConfig = {
-      web = {
-        browser = "${firefox}/bin/firefox";
+      web = { browser = "${firefox}/bin/firefox"; };
+      core = { editor = "nvim"; };
+      pull = { rebase = false; };
+      url = {
+        "ssh://git@github.com:" = { insteadOf = "https://github.com/"; };
       };
-      core = {
-        editor = "nvim";
-      };
+      init = { defaultBranch = "main"; };
+    };
+    diff-so-fancy = {
+      enable = true;
+      markEmptyLines = true;
     };
   };
 }
