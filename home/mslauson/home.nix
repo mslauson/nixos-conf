352ae6852a351afb518f6ebce029a55abc98fc8e
Matthew Slauson <matthew@slauson.io>
2024-02-24 13:58:38 -0600

    change back

--- home/mslauson/home.nix
+++ home/mslauson/home.nix
@@ -75,18 +75,37 @@
   programs.zsh = (pkgs.callPackage ./apps/zsh.nix { }).programs.zsh;
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs;
-    let
-      devPackages = import ./packages/development.nix;
-      applicationPackages = import ./packages/applications.nix;
-    in
-    devPackages ++ toolPackages ++ appPackages ++ [
-      # GTK Theme
-      catppuccin-gtk
+  home.packages = with pkgs; [
+    jq
+    lazygit
+    lazydocker
+    nixfmt
+    golines
+    docker-compose
 
-      # Overrides and scripts
-      # ... your existing overrides and scripts
-    ];
+    insomnia
+    jetbrains-toolbox
+    bitwarden
+    firefox
+    steam
+    discord
+    heroic
+
+    #GTK
+    catppuccin-gtk
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -94,7 +113,7 @@
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;    # ".screenrc".source = dotfiles/screenrc;    # ".screenrc".source = dotfiles/screenrc;    # ".screenrc".source = dotfiles/screenrc;    # ".screenrc".source = dotfiles/screenrc;
+    # ".screenrc".source = dotfiles/screenrc;    # ".screenrc".source = dotfiles/screenrc;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
