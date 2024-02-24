e1b7618e904cdfb5d131b1c6f75319f9b5112590
Matthew Slauson <matthew@slauson.io>
2024-02-24 13:05:53 -0600

    ud

--- home/mslauson/apps/zsh.nix
+++ home/mslauson/apps/zsh.nix
@@ -11,17 +11,17 @@
     zplug = {
       enable = true;
       plugins = [
-        "zsh-users/zsh-autosuggestions"
-        "zsh-users/zsh-completions"
-        "zsh-users/zsh-syntax-highlighting"
-        "Aloxaf/fzf-tab"
-        "akash329d/zsh-alias-finder"
-        "KulkarniKaustubh/fzf-dir-navigator"
-        "joshskidmore/zsh-fzf-history-search"
-        "reegnz/jq-zsh-plugin"
+        { name="zsh-users/zsh-autosuggestions"; }
+        { name="zsh-users/zsh-completions"; }
+        { name="zsh-users/zsh-syntax-highlighting"; }
+        { name="Aloxaf/fzf-tab"; }
+        { name="akash329d/zsh-alias-finder"; }
+        { name="KulkarniKaustubh/fzf-dir-navigator"; }
+        { name="joshskidmore/zsh-fzf-history-search"; }
+        { name="reegnz/jq-zsh-plugin"; }
 
-        "ChrisPenner/session-sauce"
-        "nix-community/nix-zsh-completions"
+        { name="ChrisPenner/session-sauce"; }
+        { name="nix-community/nix-zsh-completions"; }
       ];
 
     };
