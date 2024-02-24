72abfa6e9429969429b8dfc3b0e62f9bc57c18ec
Matthew Slauson <matthew@slauson.io>
2024-02-24 15:38:48 -0600

    ffs

---
+++ home/mslauson/apps/kitty.nix
@@ -0,0 +1,25 @@
+{
+
+  programs.kitty = {
+    enable = true;
+
+    font = {
+      name = "JetBrainsMono";
+      size = 11;
+    };
+    settings = {
+      window_padding_width = 12;
+      term = "xterm-256color";
+      background_opacity = "0.97";
+      hide_window_decorations = false;
+      confirm_os_window_close = 0;
+    };
+    keybindings = {
+      "ctrl+shift+c" = "copy_to_clipboard";
+      "super+c" = "copy_to_clipboard";
+      "ctrl+shift+v" = "paste_from_clipboard";
+      "super+v" = "paste_from_clipboard";
+    };
+    theme = "Catppuccin-Mocha";
+  };
+}
