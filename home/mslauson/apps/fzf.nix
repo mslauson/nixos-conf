62fad6889c2c10af02b182414e5ed5af5733bf7d
Matthew Slauson <matthew@slauson.io>
2024-02-24 21:37:39 -0600

    add fzf

---
+++ home/mslauson/apps/fzf.nix
@@ -0,0 +1,18 @@
+{
+  programs.fzf = {
+    enable = true;
+    colors = {
+      bg = "#1e1e1e";
+      "bg+" = "#1e1e1e";
+      fg = "#d4d4d4";
+      "fg+" = "#d4d4d4";
+      spinner = "#f5e0dc";
+      hl = "#f38ba8";
+      info = "#cba6f7";
+      pointer = "#f5e0dc";
+      marker = "#f5e0dc";
+      prompt = "#cba6f7";
+      "hl+" = "#f38ba8";
+    };
+  };
+}
