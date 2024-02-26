d16d89e47397207403a2fcdd386bfd51bc3ea295
Your Name <you@example.com>
2024-02-26 16:41:39 -0600

    fix mount

--- os/common/fs.nix
+++ os/common/fs.nix
@@ -7,14 +7,14 @@
     device = "gertrude:/mnt/liddy-tiddy/media";
     options = [
       "_netdev"
-      "noautox-systemd"
+      "noauto"
       "x-systemd.automount"
       "x-systemd.mount-timeout=10"
       "timeo=14"
       "x-systemd.idle-timeout=1min"
-      "uid=0"
-      "gid=3002"
-      "umask=002"
+      # "uid=0"
+      # "gid=3002"
+      # "umask=002"
     ];
   };
 
