887115546f346a7f4671da69233f4a051a4a5608
Your Name <you@example.com>
2024-02-26 16:29:01 -0600

    move

---
+++ os/common/fs.nix
@@ -0,0 +1,21 @@
+{
+
+  networking.hosts = { "10.7.5.60" = [ "gertrude" ]; };
+
+  fileSystems."/mnt/media" = {
+    fsType = "nfs";
+    device = "gertrude:/mnt/liddy-tiddy/media";
+    options = [
+      "_netdev"
+      "noautox-systemd"
+      "x-systemd.automount"
+      "x-systemd.mount-timeout=10"
+      "timeo=14"
+      "x-systemd.idle-timeout=1min"
+      "uid=0"
+      "gid=3002"
+      "umask=002"
+    ];
+  };
+
+}
