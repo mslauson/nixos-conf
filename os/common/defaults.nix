78103a9fcb18380d572d5339ba2720ddb5e64eb6
Matthew Slauson <matthew@slauson.io>
2024-02-27 11:47:47 -0600

    fix

--- os/common/defaults.nix
+++ os/common/defaults.nix
@@ -3,6 +3,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  networking = { networkmanager = { enable = true; }; };
   time.timeZone = "America/Chicago";
 
   # Select internationalisation properties.
